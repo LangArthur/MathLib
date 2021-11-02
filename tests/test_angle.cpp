cdac3c9cc5fb2b26a492555cb058ea22d6e94f46
alang <arthur.lang@epitech.eu>
2021-11-02 20:37:53 +0100

    add more test for angle :warning: need to be fix

--- tests/test_angle.cpp
+++ tests/test_angle.cpp
@@ -9,8 +9,16 @@
 
 #include "angle.hpp"
 
+// TODO fix this test
 TEST(TestAngle, UnitConvertion) {
-    std::cout << (angle::toRadian(60) == 1.0472) << std::endl;
-    ASSERT_EQ(angle::toRadian(60), 1.0472f);
+    // radian
+    ASSERT_EQ(angle::toRadian(60), M_PI / 3);
     ASSERT_EQ(angle::toRadian(90), M_PI / 2);
+    ASSERT_EQ(angle::toRadian(360), M_PI * 2);
+    ASSERT_EQ(angle::toRadian(0), 0);
+    // degree
+    ASSERT_EQ(angle::toDegree(M_PI), 180);
+    ASSERT_EQ(angle::toDegree(0), 0);
+    ASSERT_EQ(angle::toDegree(M_PI / 4), 45);
+    ASSERT_EQ(angle::toDegree(3 * M_PI / 2), 270);
 }
